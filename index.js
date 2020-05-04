3e9836d472cb8f498d05d8217e519da6eb11ccaa
LisaHJung <lisahjung@gmail.com>
2020-05-04 17:26:47 -0600

    HTML, CSS, JS connected and forms have been created

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 console.log("index.js is connected")
+
+const dropdown_season = document.getElementById("dropdown_season")
+const dropdown_travel_party = document.getElementById("dropdown_travel_party")
+
+fetch("http://localhost:3000/destinations")
+.then(response => response.json())
+.then(destinations => destinations.map(destination => {
+    const h1 = document.createElement("h1")
+    h1.innerText = destination.name
+    document.body.appendChild(h1)
+    
+}))
+
+fetch("http://localhost:3000/destinations")
+.then(response => response.json())
+.then(destinations => destinations.map(destination => {
+    const option_season = document.createElement('option')
+    option_season.innerText = destination.season 
+    option_season.value = destination.id 
+    dropdown_season.appendChild(option_season)
+
+    const option_travel_party = document.createElement('option')
+    option_travel_party.innerText = destination.travel_party
+    option_travel_party.value = destination.id 
+    dropdown_travel_party.appendChild(option_travel_party)
+
+}))
